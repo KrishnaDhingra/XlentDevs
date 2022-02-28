48ccb65d6cd99db66db5100e46c0dcf43034c79a
Krishna Dhingra <dkrishna1608@gmail.com>
2022-03-01 00:27:21 +0530

    Made the navbar responsive now just have to implement the on and off functionality with animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ import Logo from './logo'
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-100 flex justify-center">
+        <nav className="bg-gray-100 flex justify-center px-6">
             <main className="py-2 flex max-w-[1350px] w-screen justify-between items-center">
                 <section className="h-[60px] w-[60px]"><Logo/></section>
-                <ul className="flex gap-8">
+                <ul className="nav-links flex gap-8">
                     <div className="flex gap-1 items-center navbar-links">
                         <AiFillHome className="text-secondary-300 text-[13px] font-medium"/>
                         <li className="text-secondary-300 text-[13px] font-medium">HOME</li>
@@ -28,7 +28,12 @@ const Navbar = () => {
                         <li className="text-secondary-300 text-[13px] font-medium">FEEDBACK</li>
                     </div>
                 </ul>
-                <input type="text" className="max-w-[250px] w-full rounded-md py-[10px] px-5 bg-primary-100 text-sm text-secondary-300 font-medium outline-none placeholder:text-secondary-300 placeholder:text-sm placeholder:font-medium" placeholder="SEARCH" />
+                <div className="w-[35px] flex sm:hidden flex-col gap-[4px]">
+                    <div className="w-full border-t-[4px] border-primary-600 rounded-full"></div>
+                    <div className="w-full border-t-[4px] border-primary-600 rounded-full"></div>
+                    <div className="w-full border-t-[4px] border-primary-600 rounded-full"></div>
+                </div>
+                <input type="text" className="max-w-[170px] lg:max-w-[250px] hidden md:inline w-full rounded-md py-[10px] px-5 bg-primary-100 text-sm text-secondary-300 font-medium outline-none placeholder:text-secondary-300 placeholder:text-sm placeholder:font-medium" placeholder="SEARCH" />
             </main>
         </nav>
     )
